eeb39176c605c63ba49f565f90a4ca2752fc6794
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-11-05 01:53:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,16 @@
 class Artist
   attr_accessor :name
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    @@all << self 
+    @@all << self
   end
 
-  def self.all 
-    @@all 
-  end 
-    
+  def self.all
+    @@all
+  end
+
   def songs
     Song.all.select{|song| song.artist == self}
   end
@@ -21,11 +21,11 @@ class Artist
 
   def add_song_by_name(name)
     song = Song.new(name)
-    song.artist = self 
-  end 
+    song.artist = self
+  end
 
-  def self.song_count 
+  def self.song_count
     Song.all.count
-  end 
+  end
 
 end
