22c925855635702f28f6eabf1e049cef7d4270cb
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-11-05 01:42:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,20 @@
 class Song
-  attr_accessor :song, :artist, :name
+  attr_accessor :artist, :name
   @@all = []
 
-  def initialize(song)
-    @song = song
-    @@all << song
+  def initialize(name)
+    @name name 
+    @@all << self 
   end
 
   def self.all
     @@all
   end
 
+  def artist_name 
+    artist.name if artist 
+  end 
+    
 
 
 end
